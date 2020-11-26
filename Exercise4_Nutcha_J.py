7f7c58c529ffe247e2b419dac113b0e6c2714f69
Jay <areakikigirls51@gmail.com>
2020-11-26 20:55:09 +0700

    Create Exercise4_Nutcha_J.py

---
+++ Exercise4_Nutcha_J.py
@@ -0,0 +1,9 @@
+E = 75.3
+B = 92.5
+CS = 69.2
+CP = 65.7	
+print("——-Your Score -—-")
+print("Foundation English : ", E)
+print("General Business : ", B)
+print("Introduction to computer Systems : ", CS)
+print("Computer Programming : ", CP)
